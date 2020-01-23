9ab73455b26e6988678694f8782b8c81f5f21298
sajal1302 <50777789+sajal1302@users.noreply.github.com>
2020-01-23 12:07:30 +0530

    2

---
+++ Day2.py
@@ -0,0 +1,20 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+    tip_percent=(meal_cost*tip_percent)/100
+    tax_percent=(meal_cost*tax_percent)/100
+    tc=meal_cost+tip_percent+tax_percent
+    print(round(tc))
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
