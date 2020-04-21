a621cedeca3a1e0ccc1f6a1c16068193527fd3a8
sajal1302 <50777789+sajal1302@users.noreply.github.com>
2020-04-21 13:25:19 +0530

    27

---
+++ Day 27.py
@@ -0,0 +1,30 @@
+
+class TestDataEmptyArray(object):
+    
+    @staticmethod
+    def get_array():
+        # complete this function
+        return []
+
+class TestDataUniqueValues(object):
+
+    @staticmethod
+    def get_array():
+        return [8,3,1,5,6]
+
+    @staticmethod
+    def get_expected_result():
+        # complete this function
+        return 2
+
+class TestDataExactlyTwoDifferentMinimums(object):
+
+    @staticmethod
+    def get_array():
+        # complete this function
+        return[1,1]
+
+    @staticmethod
+    def get_expected_result():
+        # complete this function
+        return 0
